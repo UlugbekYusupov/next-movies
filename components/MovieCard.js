574a34615842baa411c9c2515fe8414c0675b82a
Ulugbek9602 <ulugbek.yusupov@hygge.software>
2023-01-20 16:57:35 +0500

    Movies poster, date, title done

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -3,8 +3,17 @@ import Image from "next/legacy/image";
 function MovieCard({ movie }) {
   console.log(movie);
   return (
-    <div className="bg-white shadow-sm roudned-md cursor-pointer">
-      <Image src={movie.poster} width={200} height={300}/>
+    <div className="bg-white shadow-sm rounded-md cursor-pointer">
+      <Image
+        src={movie.poster}
+        width={250}
+        height={300}
+        className="rounded-t-md"
+      />
+      <div className="p-4 py-2">
+        <h1 className="text-gray-700 font-bold text-xl">{movie.title}</h1>
+        <p className="text-gray-500 text-sm">{movie.year}</p>
+      </div>
     </div>
   );
 }
--- components/Movies.js
+++ components/Movies.js
@@ -4,9 +4,11 @@ const Movies = ({ movies }) => {
   return (
     <div className="bg-gray-700 container max-w-7xl mx-auto pb-10 px-4">
       <h1 className="text-white text-2xl mt-8 mb-5">Movies</h1>
-      {movies.map((movie) => (
-        <MovieCard movie={movie} key={movie.id} />
-      ))}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
     </div>
   );
 };
